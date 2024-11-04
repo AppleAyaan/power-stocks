eb2be5fc225a12f010ce73ce22d292a91d945de2
Ayaan Faisal <63867307+AppleAyaan@users.noreply.github.com>
2024-11-03 23:11:01 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // server accessible on local network
-  }
+  root: './',
 })
